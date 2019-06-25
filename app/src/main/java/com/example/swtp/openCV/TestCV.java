d233231c2bdf4e2bdabc1ce08777e3b4f16540e7
Christoph Schimpf <christoph.schimpf@mni.thm.de>
2019-06-25 18:40:35 +0200

    openCV eingebunden

---
+++ app/src/main/java/com/example/swtp/openCV/TestCV.java
@@ -0,0 +1,14 @@
+package com.example.swtp.openCV;
+
+import org.opencv.core.Rect;
+import org.opencv.core.Rect2d;
+
+public class TestCV {
+    Rect rect = new Rect();
+    Rect2d rect2d = new Rect2d();
+
+    public void main(){
+        rect2d.clone();
+        rect.x = 5;
+    }
+}
